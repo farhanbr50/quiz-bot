7fc25bc570547753c78dc5f9d0823691e965126c
farhanbr50 <farhanbr50@gmail.com>
2026-06-09 01:27:46 +0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -7,15 +7,15 @@ WELCOME_TEXT = """
 👤 ɴᴀᴍᴇ   ::  **{user_name}**
 ⚡ sᴛᴀᴛᴜs  ::  `READY TO RUMBLE! ⚔️`
 
-🚀 *Aapka high-speed professional quiz group mein swagat hai! Dil thaam ke baithiye...*
+🚀 *Aapka high-speed professional quiz group mein swagat hai!*
 
 📊 **[ QUIZ COMMANDS ]**
 ▶️  `/quiz/on`   —  *New Quiz Game shuru karein*
 🛑  `/quiz/off`  —  *Chal rahe game ko beech mein rokein*
-⚙️  `/sc <seconds>` — *Game ki speed badlein (e.g. /sc 10)*
+⚙️  `/sp <seconds>` — *Game ki speed badlein (e.g. /sp 10)*
 
 📐 ᴄᴀᴛᴇɢᴏʀʏ ::  🔢 **MATHS + ENGLISH 📝**
 
-✨ ═════════════════════════════ 
-✨
+✨ ═════════════════════════════ ✨
+
 """
